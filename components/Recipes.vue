417d40785dd7b17c9d888c682d765e5c84a22a48
Akash Bhosale <MY_NAME@example.com>
2022-07-26 20:28:04 +0530

    ..

---
+++ components/Recipes.vue
@@ -0,0 +1,84 @@
+<template>
+  <article>
+    <div class="card" v-for="recipe in recipeArray" :key="recipe.id">
+      <div class="card-img">
+        <img :src="recipe.image" alt="title" />
+        <div class="card-bottom">
+          <h3>{{ recipe.title }}</h3>
+          <p v-html="recipe.summary"></p>
+          <button @click="loadRecipe">More info</button>
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  methods: {
+    loadRecipe() {
+      console.log("first");
+    },
+  },
+  props: ["recipeArray"],
+};
+</script>
+
+<style>
+.card {
+  width: 250px;
+  height: 450px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 4px 4px 25px rgba(0, 0, 0, 0.1);
+  transition: 0.3s all ease;
+  cursor: pointer;
+}
+
+.card:hover {
+  transform: scale(1.025);
+}
+
+.card-img {
+  height: 220px;
+  width: 100%;
+}
+.card-img img {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  object-position: center;
+}
+.card-bottom {
+  padding: 8px;
+  position: relative;
+  height: 230px;
+}
+
+.card-bottom button {
+  cursor: pointer;
+  margin: 0 auto;
+  padding: 5px 10px;
+  font-size: 20px;
+  outline: none;
+  border: none;
+  background: lightslategrey;
+  border-radius: 8px;
+  color: white;
+  position: absolute;
+  bottom: 25px;
+}
+.card-bottom p {
+  height: 80px;
+  margin: 10px 0;
+  overflow: hidden;
+}
+
+article {
+  display: flex;
+  padding: 20px 10px;
+  gap: 20px;
+  flex-wrap: wrap;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <main>
+    <h1 class="main-title">Recipe Search</h1>
+    <form @submit.prevent="fetchData">
+      <input
+        type="text"
+        v-model="query"
+        placeholder="Enter a recipe to search"
+      />
+      <button type="submit">Search</button>
+    </form>
+    <Recipes :recipeArray="recipes" />
+  </main>
+</template>
+
+<script>
+import axios from "axios";
+import Recipes from "../components/Recipes";
+export default {
+  components: {
+    Recipes,
+  },
+  data() {
+    return {
+      recipes: [],
+      query: "",
+    };
+  },
+  methods: {
+    async fetchData() {
+      let res = await axios.get(
+        `https://api.spoonacular.com/recipes/complexSearch?query=${this.query}&addRecipeInformation=true&addRecipeNutrition=true&number=20&apiKey=${process.env.API_KEY}`
+      );
+      this.recipes = res.data.results;
+      console.log(this.recipes);
+    },
+  },
+};
+</script>
+
+<style>
+.main-title {
+  text-align: center;
+  margin: 20px 0;
+}
+
+form {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 300px;
+  margin: 0 auto;
+}
+
+form input {
+  width: 100%;
+  height: 40px;
+  font-size: 24px;
+  outline: none;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  text-indent: 10px;
+  margin-bottom: 15px;
+}
+
+form button {
+  cursor: pointer;
+  margin: 0 auto;
+  width: 50%;
+  height: 40px;
+  font-size: 24px;
+  outline: none;
+  border: none;
+  background: lightslategrey;
+  border-radius: 8px;
+  color: white;
+  margin-bottom: 15px;
+}
+</style>
